afbf025ce90cd59b622b95cc9c8636ecdeff08b5
Lael Arriaga <lael.arriaga@gmail.com>
2021-05-17 19:02:32 -0500

    Added ConsoleApp client for dev purposes.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.Models;
+using Common.Services;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public string FileName => "ATD_AX_GAL.MIR";
+
+        static void Main(string[] args)
+        {
+            var lines = FileProcessor.GetLinesFromFile("ATD_AX_GAL.MIR");
+            var segmentList = FileProcessor.BuildFileSegments(lines);
+            var MIRSegments = SegmentProcessor.GenerateAllSegments(segmentList);
+            foreach(var segment in MIRSegments)
+                Console.Write(segment.ToString());
+            Console.ReadLine();
+        }
+    }
+}
