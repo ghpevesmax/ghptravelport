28aad201a560dfb6141cb6727d8e47238c5a8801
Lael Arriaga <lael.arriaga@gmail.com>
2021-10-04 21:02:34 -0500

    Common.Helpers/FileHelper:  Added to manage file extension processes

---
+++ Common/Helpers/FileHelper.cs
@@ -0,0 +1,91 @@
+﻿using Common.Utils;
+using System.IO;
+
+namespace Common.Helpers
+{
+    public static class FileHelper
+    {
+        public static string MoveFileToApiError(string sourceFileFullName)
+        {
+            string newFileFullName;
+            var fileDestName = sourceFileFullName
+                .RemoveProcessExtension()
+                .AddApiErrorExtension();
+
+            if (!File.Exists(fileDestName))
+            {
+                newFileFullName = sourceFileFullName
+                    .RemoveProcessExtension()
+                    .AddApiErrorExtension();
+
+                Directory.Move(sourceFileFullName, newFileFullName);
+            }
+            else
+            {
+                newFileFullName = sourceFileFullName
+                    .RemoveProcessExtension()
+                    .RemoveMIRFileExtension()
+                    .AddTicks()
+                    .AddMIRFileExtension()
+                    .AddApiErrorExtension();
+                Directory.Move(sourceFileFullName, newFileFullName);
+            }
+            return newFileFullName;
+        }
+
+        public static string MoveFileToProcess(string sourceFileFullName)
+        {
+            string newFileFullName;
+            var fileDestName = sourceFileFullName
+                .RemoveStageExtension()
+                .AddProcessExtension();
+
+            if (!File.Exists(fileDestName))
+            {
+                newFileFullName = sourceFileFullName
+                    .RemoveStageExtension()
+                    .AddProcessExtension();
+                Directory.Move(sourceFileFullName, newFileFullName);
+            }
+            else
+            {
+                newFileFullName = sourceFileFullName
+                    .RemoveStageExtension()
+                    .RemoveMIRFileExtension()
+                    .AddTicks()
+                    .AddMIRFileExtension()
+                    .AddProcessExtension();
+                Directory.Move(sourceFileFullName, newFileFullName);
+            }
+            return newFileFullName;
+        }
+
+        public static string MoveFileToProcessed(string sourceFileFullName)
+        {
+            string newFileFullName;
+            var fileDestName = sourceFileFullName
+                .RemoveProcessExtension()
+                .AddProcessedExtension();
+
+            if (!File.Exists(fileDestName))
+            {
+                newFileFullName = sourceFileFullName
+                    .RemoveProcessExtension()
+                    .AddProcessedExtension();
+
+                Directory.Move(sourceFileFullName, newFileFullName);
+            }
+            else
+            {
+                newFileFullName = sourceFileFullName
+                    .RemoveProcessExtension()
+                    .RemoveMIRFileExtension()
+                    .AddTicks()
+                    .AddMIRFileExtension()
+                    .AddProcessedExtension();
+                Directory.Move(sourceFileFullName, newFileFullName);
+            }
+            return newFileFullName;
+        }
+    }
+}
