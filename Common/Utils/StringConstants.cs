a21f21c837aba27069d65f7b0096ffe6e17fa73a
Lael Arriaga <lael.arriaga@gmail.com>
2021-10-04 21:03:17 -0500

    Common.Utils: Added to help extension processes

---
+++ Common/Utils/StringConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace Common.Utils
+{
+    public static class StringConstants
+    {
+        public static readonly string BrandName = "ghptravelport";
+        public static readonly string ApiErrorFileExtension = ".aerr";
+        public static readonly string DupFileExtension = ".dup";
+        public static readonly string MIRFileExtension = ".MIR";
+        public static readonly string MIRPathExtension = "*.MIR";
+        public static readonly string MIRStagePathExtension = "*.MIR.stage";
+        public static readonly string ProcessFileExtension = ".process";
+        public static readonly string ProcessedFileExtension = ".processed";
+        public static readonly string Stage = "stage";
+        public static readonly string StageExtension = ".stage";
+    }
+}
---
+++ Common/Utils/StringExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Common.Utils
+{
+    public static class StringExtensions
+    {
+        public static string AddApiErrorExtension(this string s) => 
+            s + StringConstants.ApiErrorFileExtension;
+
+        public static string AddDupExtension(this string s) => 
+            s + StringConstants.DupFileExtension;
+
+        public static string AddMIRFileExtension(this string s) =>
+            s + StringConstants.MIRFileExtension;
+
+        public static string AddProcessExtension(this string s) => 
+            s + StringConstants.ProcessFileExtension;
+
+        public static string AddProcessedExtension(this string s) => 
+            s + StringConstants.ProcessedFileExtension;
+
+        public static string AddStageExtension(this string s) => 
+            s + StringConstants.StageExtension;
+
+        public static string AddTicks(this string s) => 
+            s + "-" + DateTime.UtcNow.TimeOfDay.Ticks.ToString();
+
+        public static string RemoveMIRFileExtension(this string s) => 
+            s.Replace(StringConstants.MIRFileExtension, string.Empty);
+
+        public static string RemoveProcessExtension(this string s) => 
+            s.Replace(StringConstants.ProcessFileExtension, string.Empty);
+
+        public static string RemoveStageExtension(this string s) => 
+            s.Replace(StringConstants.StageExtension, string.Empty);
+
+    }
+}
