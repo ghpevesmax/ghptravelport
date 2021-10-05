cd7898cf8a9dd89070382300cb98659879dcc98d
Lael Arriaga <lael.arriaga@gmail.com>
2021-10-04 21:00:59 -0500

    ConsoleApp: Code enhanced to copy contents to the clipboard as well as get the system directory for support dev purpose

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,22 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Common.Models;
-using Common.Services;
+﻿using Common.Services;
+using System;
+using System.Text;
+using TextCopy;
 
 namespace ConsoleApp
 {
     class Program
     {
-        public string FileName => "ATD_AX_GAL.MIR";
+        public static string FileName => "ATD_AX_GAL.MIR";
 
+        [STAThread]
         static void Main(string[] args)
         {
-            var lines = FileProcessor.GetLinesFromFile("ATD_AX_GAL.MIR");
+
+            var lines = FileProcessor.GetLinesFromFile(FileName);
             var segmentList = FileProcessor.BuildFileSegments(lines);
             var MIRSegments = SegmentProcessor.GenerateAllSegments(segmentList);
-            foreach(var segment in MIRSegments)
+            var clipboard = new StringBuilder();
+            foreach (var segment in MIRSegments)
+            {
+                clipboard.AppendLine(segment.ToString());
                 Console.Write(segment.ToString());
+            }
+            ClipboardService.SetText(clipboard.ToString());
+            Console.Write(Environment.SystemDirectory);
             Console.ReadLine();
         }
     }
