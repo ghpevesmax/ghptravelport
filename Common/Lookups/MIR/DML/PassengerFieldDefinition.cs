d779874bdf767e9581093ad4fe14b3188768a1a6
Lael Arriaga <lael.arriaga@gmail.com>
2021-11-27 18:59:51 -0600

    Refactored constructor references

--- Common/Lookups/MIR/DML/PassengerFieldDefinition.cs
+++ Common/Lookups/MIR/DML/PassengerFieldDefinition.cs
@@ -9,38 +9,38 @@ namespace Common.Lookups
     public static class PassengerFieldDefinition
     {
         public static FieldDefinition A02SEC =>
-            new FieldDefinition
+            new()
             {
                 Name = "A02SEC",
                 SegmentPosition = 0,
                 Length = 3,
             };
         public static FieldDefinition A02NME =>
-            new FieldDefinition
+            new()
             {
                 Name = "A02NME",
                 SegmentPosition = 3,
                 Length = 33,
             };
         public static FieldDefinition A02TRN =>
-            new FieldDefinition
+            new()
             {
                 Name = "A02TRN",
                 SegmentPosition = 36,
                 Length = 11,
             };
         public static FieldDefinition A02TIN =>
-            new FieldDefinition
+            new()
             {
                 Name = "A02TIN",
                 SegmentPosition = 47,
                 Length = 22,
                 NestedFields = new FieldDefinition[4]
                 {
-                    new FieldDefinition{ Name = "A02YIN", SegmentPosition = 47, Length = 1 },
-                    new FieldDefinition{ Name = "A02TKT", SegmentPosition = 48, Length = 10 },
-                    new FieldDefinition{ Name = "A02NBK", SegmentPosition = 58, Length = 2 },
-                    new FieldDefinition{ Name = "A02INV", SegmentPosition = 60, Length = 9 },
+                    new(){ Name = "A02YIN", SegmentPosition = 47, Length = 1 },
+                    new(){ Name = "A02TKT", SegmentPosition = 48, Length = 10 },
+                    new(){ Name = "A02NBK", SegmentPosition = 58, Length = 2 },
+                    new(){ Name = "A02INV", SegmentPosition = 60, Length = 9 },
                 }
             };
         public static int CarriageReturnNumber => 3;
